83f1035631dfa3de07182c49efc658e26554a1c8
Alina Moca <146835864+AlinaMoca25@users.noreply.github.com>
2025-04-12 19:38:48 +0300

    added mandatory spendings

---
+++ app/Filament/Admin/Resources/MandatorySpendingResource/Pages/CreateMandatorySpending.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Filament\Admin\Resources\MandatorySpendingResource\Pages;
+
+use App\Filament\Admin\Resources\MandatorySpendingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateMandatorySpending extends CreateRecord
+{
+    protected static string $resource = MandatorySpendingResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+{
+    $data['user_id'] = auth()->id();
+    return $data;
+}
+}
